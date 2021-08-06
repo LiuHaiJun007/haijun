21b98fa6d10df93645e633e3a00192eba91bcec7
liuhaijun <714779582@qq.com>
2021-08-06 13:22:54 +0800

    添加文件

--- xh_spring09/src/main/java/cn/xh/dao/IAccountDao.java
+++
@@ -1,8 +0,0 @@
-package cn.xh.dao;
-
-import cn.xh.pojo.Account;
-
-public interface IAccountDao {
-    Account findAccountByName(String name);
-    void updateAccount(Account account);
-}
--- xh_spring09/src/main/java/cn/xh/service/IAccountService.java
+++
@@ -1,5 +0,0 @@
-package cn.xh.service;
-
-public interface IAccountService {
-    void transfer(String sourceAccount,String targetAccount,float money);
-}
